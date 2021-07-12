6aa7eb73fd7222a7a0bf6eabc33b0625a8a983f7
Pranav Malvawala <42204892+pranavmalvawala@users.noreply.github.com>
2021-07-12 16:25:55 +0530

    feat: run database init script only on first start-up (#12)

---
+++ Images/CoreApis/MessagingApi/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This script checks if the container is started for the first time.
+
+CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
+if [ ! -e /$CONTAINER_ALREADY_STARTED ]; then
+    touch /$CONTAINER_ALREADY_STARTED
+    echo "-- First container startup --"
+    # place your script that you only want to run on first startup.
+    npm run initdb && npm run dev
+else
+    echo "-- Not first container startup --"
+    # script that should run the rest of the times (instances where you 
+    # stop/restart containers).
+    npm run dev
+fi
